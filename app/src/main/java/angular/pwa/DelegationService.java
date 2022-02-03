f0defb5279f226c139f4ad9fa4e7f2ba041d4e6c
Jeremy Chaudourne <jchaudourne@protonmail.com>
2022-02-03 13:27:11 +0100

    first commit

---
+++ app/src/main/java/angular/pwa/DelegationService.java
@@ -0,0 +1,14 @@
+package angular.pwa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
